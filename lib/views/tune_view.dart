4ace73cc8a155a1f2f53bcb46f5a3f7f02861a0f
bedaoui khalil <k.bedaoui@bekerlaboratoires.com>
2024-06-07 16:23:16 +0100

    init project && AppBar

---
+++ lib/views/tune_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TuneView extends StatelessWidget {
+  const TuneView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Flutter Tune'),
+        backgroundColor: const Color(0xff253238),
+      ),
+    );
+  }
+}
