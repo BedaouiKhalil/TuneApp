dbf7d7bbf5cde47f06ca0e054e47c2f20770f950
bedaoui khalil <k.bedaoui@bekerlaboratoires.com>
2024-06-07 23:29:49 +0100

    end v1

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:tune/models/tune_model.dart';
+import 'package:tune/widgets/tune_item.dart';
 
 class TuneView extends StatelessWidget {
-  const TuneView({super.key});
+ const TuneView({super.key});
+
+ final List<TuneModel> tunesColor = const [
+    TuneModel(color: Color(0xffF44336), sound: 'note1.wav'),
+    TuneModel(color: Color(0xffF89800), sound: 'note2.wav'),
+    TuneModel(color: Color(0xffFEEB3B), sound: 'note3.wav'),
+    TuneModel(color: Color(0xff4CAF50), sound: 'note4.wav'),
+    TuneModel(color: Color(0xff2F9688), sound: 'note5.wav'),
+    TuneModel(color: Color(0xff2896F3), sound: 'note6.wav'),
+    TuneModel(color: Color(0xff9C27B0), sound: 'note7.wav'),
+ ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +22,13 @@ class TuneView extends StatelessWidget {
         centerTitle: true,
         title: const Text('Flutter Tune'),
         backgroundColor: const Color(0xff253238),
+      ),
+      body: Column(
+        children: tunesColor
+            .map(
+              (e) => TuneItem(tune: e),
+            )
+            .toList(),
       ),
     );
   }
